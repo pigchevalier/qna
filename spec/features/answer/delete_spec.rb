8c19307d8d3c3052ea6ebaef483fa29775439387
pigchevalier <nadine.boudkina@gmail.com>
2021-12-27 15:06:05 +0300

    change author_of, unauth user delete specs, have_link

--- spec/features/answer/delete_spec.rb
+++ spec/features/answer/delete_spec.rb
@@ -26,8 +26,14 @@ feature 'Author can delete answer', %q{
       sign_in(user_not_author)          
       visit question_path(id: answer.question)
 
-      expect(page).to_not have_content "Delete answer"
+      expect(page).to_not have_link "Delete answer"
     end
   end
+  describe 'Unauth. user' do  
+    scenario 'tries to delete answer' do        
+      visit question_path(id: answer.question)
 
+      expect(page).to_not have_link "Delete answer"
+    end
+  end
 end
